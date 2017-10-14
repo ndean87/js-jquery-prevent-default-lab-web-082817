a2810442b065feebaee4cad4fb7f8813f6d1ebc0
ndean87 <ndean87@gmail.com>
2017-10-14 18:19:20 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,23 @@
-$(document).ready(function(){
-  // call functions here
-});
+window.onload = () => {
+	const submit = document.getElementsByClassName('submit-button')[0];
+	const list = document.getElementsByClassName('list-items')[0];
+	const error = document.getElementsByClassName('error')[0];
 
-// define functions here
+	submit.addEventListener('click', event => {
+		event.preventDefault();
+		const inputValue = document.getElementsByClassName('input-field')[0].value;
+		const categoryValue = document.getElementsByClassName('category')[0].value;
+
+		if (!inputValue) {
+			error.innerHTML = 'You didnt enter a task!';
+		} else if (!categoryValue) {
+			error.innerHTML = 'You didnt choose a category!';
+		} else {
+			error.innerHTML = '';
+			const li = document.createElement('li');
+			li.innerHTML = `${categoryValue}: ${inputValue}`;
+			list.appendChild(li);
+			document.getElementsByClassName('input-field')[0].value = '';
+		}
+	});
+};
